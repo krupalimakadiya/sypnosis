93704d7eb465c31fc931da4f7cb42d826ae35873
krupali makadiya <krupalimakadiya123@gmail.com>
2020-05-24 09:41:57 +0530

    Updates

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to sypnosis2!');
+    expect(page.getTitleText()).toEqual('Welcome to sypnosis!');
   });
 
   afterEach(async () => {
